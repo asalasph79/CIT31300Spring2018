c6306cbf184deff9ef60cf4e4ec38043ee7a8449
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-01-16 19:58:56 -0500

    Add files via upload

---
+++ Ex1/header.inc.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Exercise 1: PHP Review</title>
+</head>
+<div id="logo">
+  <img src="../images/logophp.jpg" width="100%">
+</div>
