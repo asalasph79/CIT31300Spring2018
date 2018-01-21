a6458c40ef3deba9bd3e73a87cb5927728d6da25
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-01-21 15:12:38 -0500

    Delete header.inc.php

--- Ex1/header.inc.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Exercise 1: PHP Review</title>
-</head>
-<div id="logo">
-  <img src="../images/logophp.jpg" width="100%">
-</div>
