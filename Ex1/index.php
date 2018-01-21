8120969bb180b77eba7c634b8454de78102397f5
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-01-21 15:15:35 -0500

    Add files via upload

---
+++ Ex1/index.php
@@ -0,0 +1,38 @@
+<?php
+	include("_includes/config.php");
+	include(ABSOLUTE_PATH . "/_includes/header.inc.php");
+?>
+ 
+
+
+
+<?php
+
+$preferences=array("<h1>Angel I Salas-Phillips</h1>"," Favorite Color = Black","Favorite Movie = Star Wars","Favorite Book = World War Z", "Favorite Website = Facebook");
+$arrlength=count($preferences);
+
+for($x=0;$x<$arrlength;$x++)
+  {
+  echo $preferences[$x];
+    echo "<ul></ul>";
+  }
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <?php
+   include(ABSOLUTE_PATH ."_includes/footer.inc.php");
+ ?>
