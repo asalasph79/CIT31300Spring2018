cc90d4da86a2d5e5130f5188609f7a8d028a9131
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-03-04 22:02:35 -0500

    Add files via upload

---
+++ a3/home.php
@@ -0,0 +1,7 @@
+<?php include('elements/header.php');?>
+<div class="container">
+	<div class="page-header">
+    <h1> Hey!!! This is a nice Home View </h1>
+  </div>
+</div>
+<?php include('elements/footer.php');?>
