f43b96d376a2f8be3da8bb70f1f9bf21be2fcf57
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-03-25 14:03:38 -0400

    Add files via upload

---
+++ ex5/home.php
@@ -0,0 +1,9 @@
+<?php
+require_once('application/config.php');
+include('elements/header.php');?>
+<div class="container">
+	<div class="page-header">
+    <h1>This is a nice home view!!!!</h1>
+  </div>
+</div>
+<?php include('elements/footer.php');?>
