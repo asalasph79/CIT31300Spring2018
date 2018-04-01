4751bdc356e61589ed2575bbcfc9548e272d4b91
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-04-01 18:29:29 -0400

    Add files via upload

---
+++ ex6/views/home/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include('views/elements/header.php');?>
+<div class="container">
+	<div class="page-header">
+    <h1>Hey!!! This is a nice view</h1>
+  </div>
+</div>
+<?php include('views/elements/footer.php');?>
+  
