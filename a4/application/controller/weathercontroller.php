dc1d08d3e617222fc50076b4c67eb7adf02c87a9
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-04-08 18:39:45 -0400

    Add files via upload

---
+++ a4/application/controller/weathercontroller.php
@@ -0,0 +1,17 @@
+<?php
+
+class WeatherController extends Controller{
+
+	public function index(){
+		$this->set(result,false);
+
+	}
+        public function getresults() {
+        $xml = simplexml_load_file("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=1491bb4dfc8d4e438ca01504171211&q=%22".$_POST['zip']."%22&format=xml&num_of_days=2");
+        $this->set('result', true);
+        $this->set('weather', $xml);
+        }
+
+}
+
+?>   
