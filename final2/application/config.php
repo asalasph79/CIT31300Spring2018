05ad87d7987128d7538218be2da65a4618764435
asalasph79 <35432175+asalasph79@users.noreply.github.com>
2018-05-05 13:29:10 -0400

    Add files via upload

---
+++ final2/application/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22281/final2/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'asalasph');
+define('DB_PASS', 'sithlord3947');
+define('DB_NAME', 'asalasph_db');
